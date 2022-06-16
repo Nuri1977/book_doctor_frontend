91b8cadd14f9933181cf1e0c68b4da570f4d6b2e
Nuri1977 <nurilacka@gmail.com>
2022-06-16 11:02:47 +0200

    Edit backend url

--- src/helpers/api/call.js
+++ src/helpers/api/call.js
@@ -1,6 +1,6 @@
 import StorageManager from '../format/StorageManager';
 
-export const endpoint = 'http://localhost:3001/v1/';
+export const endpoint = 'https://book-appointments-microverse.herokuapp.com/v1/';
 
 export const formatDate = (date) => date;
 
